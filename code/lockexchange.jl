7c8a96a1f16e205e6928b2b49d2e2ce895d62a6f
iury simoes-sousa <simoesiury@gmail.com>
2022-03-11 21:58:19 -0500

    initial commit for the model

--- code/lockexchange.jl
+++ code/lockexchange.jl
@@ -1,1 +1,34 @@
+using Plots
+using Oceananigans
+using Oceananigans.Units
 
+
+L = 10meters
+H = 4meters
+
+Ny = Int(2*L/0.1) # number of points in y
+Nz = Int(H/0.1) # number of points in z
+
+grid = RectilinearGrid(
+    size=(Ny, Nz), 
+    y=(-L*meters, L*meters), 
+    z=(-H*meters,0),
+    topology=(Flat, Bounded, Bounded)
+)
+
+closure = SmagorinskyLilly()
+
+model = NonhydrostaticModel(grid=grid, closure=closure, buoyancy=SeawaterBuoyancy(), tracers=(:T, :S))
+
+initial_temperature(x, y, z) = 20 + 0.5*y/abs(y)
+set!(model, T=initial_temperature, S=35)
+
+simulation = Simulation(model, Δt = 0.1seconds, stop_time = 10minutes)
+
+simulation.output_writers[:temperature] =
+    JLD2OutputWriter(model, model.tracers, prefix = "../data/lockexchange",
+                     schedule=TimeInterval(1minute), force = true)
+
+run!(simulation)
+
+T = FieldTimeSeries("../data/lockexchange.jld2", "T")
